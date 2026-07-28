f44f1597b3c88eb100b5d6f6c1bf742ca2f03936
cian546 <wjwypm897z@privaterelay.appleid.com>
2026-07-28 22:02:58 +0100

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -23,7 +23,7 @@ export const botConfig = {
     // 5 = Competing
     activities: [
       {
-        name: "Custom Status", // required by Discord API, not shown in the client
+        name: "Watching Flordia State Roleplay", // required by Discord API, not shown in the client
         state: "Watching Flordia State Roleplay",     // this is what people actually see
         type: 3,               // Custom
       },
